e9a105e49e76225c8ce1236eaf40b86eb0f5f0df
noam-elbaz <nelbaz@gmail.com>
2022-08-10 23:14:42 +0300

    supbase auth fix 3

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,3 +1,3 @@
 import { createClient } from "@supabase/supabase-js";
 
-export default createClient(process.env.SUPABASE_KEY, process.env.SUPABASE_URL);
+export default createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
