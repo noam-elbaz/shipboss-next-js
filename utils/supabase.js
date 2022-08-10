69155920820460297477297bdf41537f6c8f552e
noam-elbaz <nelbaz@gmail.com>
2022-08-10 23:21:40 +0300

    fix 5

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,6 +1,3 @@
 import { createClient } from "@supabase/supabase-js";
 
-export default createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY
-);
+export default createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
